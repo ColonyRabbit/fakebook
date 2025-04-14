8db76e374842f3b0d4d7a93f6935f83e5d493a9b
khanakorn <khanakorn01@gmail.com>
2025-04-14 17:01:08 +0700

    .

--- prisma/migrations/20250413092544_add_content_to_posts/migration.sql
+++
@@ -1,2 +0,0 @@
--- RenameIndex
-ALTER INDEX "Follower_followerId_followingId_key" RENAME TO "unique_followerId_followingId";
--- prisma/migrations/20250413150150_update/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "Like_userId_idx" ON "Like"("userId");
-
--- CreateIndex
-CREATE INDEX "Like_postId_idx" ON "Like"("postId");
