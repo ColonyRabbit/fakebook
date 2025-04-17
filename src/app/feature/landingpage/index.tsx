13ac466d90b8c161000a085586cf67b47e8d9d1a
khanakorn <khanakorn01@gmail.com>
2025-04-17 15:54:19 +0700

    .

--- src/app/feature/landingpage/index.tsx
+++ src/app/feature/landingpage/index.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import PostCreator from "./components/PostCreator";
-import Feet from "./components/Feet";
+import Feed from "./components/Feed";
 
 const IndexLandingPage = () => {
   return (
     <div className="flex flex-col   light:bg-gray-100">
       <PostCreator />
-      <Feet />
+      <Feed />
     </div>
   );
 };
