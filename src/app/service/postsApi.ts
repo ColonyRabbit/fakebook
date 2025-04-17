3f815301429d288a3d21c13fcf759a834b03bdc2
khanakorn <khanakorn01@gmail.com>
2025-04-17 13:20:53 +0700

    .

--- src/app/service/postsApi.ts
+++ src/app/service/postsApi.ts
@@ -1,25 +1,40 @@
+// service/postsApi.ts
+
 import { IResIResponsePostsType } from "../type/postType";
 
-// postsApi.ts
 const postsApi = (() => {
   const apiBaseUrl = "/api";
-
   const getAllPosts = async (): Promise<IResIResponsePostsType> => {
-    const response = await fetch(`${apiBaseUrl}/posts`, {
-      method: "GET",
+    const res = await fetch(`${apiBaseUrl}/posts`);
+    if (!res.ok) throw new Error("Failed to fetch posts");
+    return (await res.json()) as IResIResponsePostsType;
+  };
+  const deleteOnePost = async (id: string): Promise<IResIResponsePostsType> => {
+    const res = await fetch(`${apiBaseUrl}/posts`, {
+      method: "DELETE",
+      body: JSON.stringify({ postId: id }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
-
-    if (!response.ok) {
-      throw new Error("postsApi failed");
-    }
-
-    const data = await response.json();
-    return data as IResIResponsePostsType;
+    if (!res.ok) throw new Error("Failed to delete post");
+    return (await res.json()) as IResIResponsePostsType;
+  };
+  const updateOnePost = async (
+    content: string,
+    postId: string
+  ): Promise<IResIResponsePostsType> => {
+    const res = await fetch(`${apiBaseUrl}/posts`, {
+      method: "PATCH",
+      body: JSON.stringify({ content, postId }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!res.ok) throw new Error("Failed to update post");
+    return (await res.json()) as IResIResponsePostsType;
   };
 
-  return {
-    getAllPosts,
-  };
+  return { getAllPosts, deleteOnePost, updateOnePost };
 })();
-
 export default postsApi;
