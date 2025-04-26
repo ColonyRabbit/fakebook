7e077036aaf16987571d1a65d11f8bf9a8683c4f
khanakorn <khanakorn01@gmail.com>
2025-04-26 17:06:37 +0700

    hard code

--- src/app/api/push/send/route.ts
+++ src/app/api/push/send/route.ts
@@ -1,42 +1,32 @@
 import { NextResponse } from "next/server";
-import admin, { ServiceAccount } from "firebase-admin";
+import admin from "firebase-admin";
 
-let firebaseAdmin: typeof import("firebase-admin");
+// 👇 สร้าง serviceAccount
+const serviceAccount = {
+  type: process.env.GOOGLE_TYPE,
+  project_id: process.env.GOOGLE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // สำคัญมาก
+  client_email: process.env.GOOGLE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_AUTH_URI,
+  token_uri: process.env.GOOGLE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+};
+
+// 👇 ต้องเช็กให้ initializeApp แค่ครั้งเดียว
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  });
+}
 
 export async function POST(req: Request) {
-  if (!firebaseAdmin) {
-    firebaseAdmin = await import("firebase-admin");
-
-    const serviceAccount = {
-      type: process.env.GOOGLE_TYPE,
-      project_id: process.env.GOOGLE_PROJECT_ID,
-      private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
-      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      client_email: process.env.GOOGLE_CLIENT_EMAIL,
-      client_id: process.env.GOOGLE_CLIENT_ID,
-      auth_uri: process.env.GOOGLE_AUTH_URI,
-      token_uri: process.env.GOOGLE_TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
-      client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
-    };
-
-    if (!firebaseAdmin.apps.length) {
-      firebaseAdmin.initializeApp({
-        credential: firebaseAdmin.credential.cert(
-          serviceAccount as admin.ServiceAccount
-        ),
-      });
-    }
-  }
-
-  const { targetFcmToken, title, body } = await req.json();
-
-  if (!targetFcmToken || !title || !body) {
-    return NextResponse.json({ error: "Missing fields" }, { status: 400 });
-  }
-
   try {
-    const messaging = firebaseAdmin.messaging();
+    const { targetFcmToken, title, body } = await req.json();
+
+    const messaging = admin.messaging();
 
     await messaging.send({
       token: targetFcmToken,
@@ -47,8 +37,8 @@ export async function POST(req: Request) {
     });
 
     return NextResponse.json({ success: true });
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error sending push:", error);
-    return NextResponse.json({ error: "Failed to send push" }, { status: 500 });
+    return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
