fb0838506d5143a0311bd1958b312ef8a1e4efd5
khanakorn <khanakorn01@gmail.com>
2025-05-05 18:53:29 +0700

    robot

---
+++ src/app/head.tsx
@@ -0,0 +1,11 @@
+// ✅ app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="TcywSLjr0baKqRMX6CwZ0gMBsHeIUfnXTQmEFrkqN8g"
+      />
+    </>
+  );
+}
