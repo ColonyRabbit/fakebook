cd07ca2d46724b78594c4faabc32fe1de1a752fa
khanakorn <khanakorn01@gmail.com>
2025-05-05 18:44:33 +0700

    robot

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://fakebook-psi-six.vercel.app/", // ← เปลี่ยนเป็น URL ของคุณ
+  generateRobotsTxt: true, // จะสร้าง robots.txt ให้อัตโนมัติ
+  sitemapSize: 7000, // default ก็ได้
+};
--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,11 +1,37 @@
+import { icons } from "lucide-react";
 import IndexProfile from "../../feature/profille/IndexProfile";
+import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../../lib/authOptions";
 
-export async function generateMetadata({ params }: { params: any }) {
-  const { id } = params;
+export async function generateMetadata() {
+  const session = await getServerSession(authOptions);
+
   return {
-    title: `${id}`,
+    title: `${session.user.name}`,
     description:
       "เชื่อมต่อ แบ่งปัน และสร้างความทรงจำกับเพื่อนๆ ของคุณบนแพลตฟอร์มโซเชียลมีเดียที่ปลอดภัยและเป็นมิตร",
+    icons: {
+      icon: session.user.photoUrl,
+    },
+    openGraph: {
+      title: `${session.user.name}`,
+      description:
+        "เชื่อมต่อ แบ่งปัน และสร้างความทรงจำกับเพื่อนๆ ของคุณบนแพลตฟอร์มโซเชียลมีเดียที่ปลอดภัยและเป็นมิตร",
+      images: [
+        {
+          url: session.user.photoUrl,
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${session.user.name}`,
+      description:
+        "เชื่อมต่อ แบ่งปัน และสร้างความทรงจำกับเพื่อนๆ ของคุณบนแพลตฟอร์มโซเชียลมีเดียที่ปลอดภัยและเป็นมิตร",
+      images: [session.user.photoUrl],
+    },
   };
 }
 
