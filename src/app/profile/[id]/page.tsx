254e2043a6a3ff7f1e49e299dc6e4df5d6f3a59f
khanakorn <khanakorn01@gmail.com>
2025-05-06 19:47:41 +0700

    ad ui

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,24 @@
+// src/app/profile/[id]/page.tsx
+import { Metadata } from "next";
 import IndexProfile from "../../feature/profille/IndexProfile";
 
-export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = params;
-  return <IndexProfile id={id} />;
+type PageProps = {
+  params: {
+    id: string;
+  };
+};
+
+// แก้ไขฟังก์ชัน generateMetadata
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  return {
+    title: `Profile of ${params.id}`,
+    description: "User profile page",
+  };
+}
+
+// ฟังก์ชันแสดงหน้าจอ
+export default async function Page({ params }: PageProps) {
+  return <IndexProfile id={params.id} />;
 }
