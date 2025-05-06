320d1ab5dcaaed70beb87edd7836c1fcb539f36a
khanakorn <khanakorn01@gmail.com>
2025-05-06 19:33:15 +0700

    ad ui

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,4 +1,40 @@
+import { icons } from "lucide-react";
 import IndexProfile from "../../feature/profille/IndexProfile";
+import { useSession } from "next-auth/react";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../../../../lib/authOptions";
+
+export async function generateMetadata() {
+  const session = await getServerSession(authOptions);
+
+  return {
+    title: `${session.user.name}`,
+    description:
+      "เชื่อมต่อ แบ่งปัน และสร้างความทรงจำกับเพื่อนๆ ของคุณบนแพลตฟอร์มโซเชียลมีเดียที่ปลอดภัยและเป็นมิตร",
+    icons: {
+      icon: session.user.photoUrl,
+    },
+    openGraph: {
+      title: `${session.user.name}`,
+      description:
+        "เชื่อมต่อ แบ่งปัน และสร้างความทรงจำกับเพื่อนๆ ของคุณบนแพลตฟอร์มโซเชียลมีเดียที่ปลอดภัยและเป็นมิตร",
+      url: `https://fakebook.com/profile/${session.user.id}`,
+      images: [
+        {
+          url: session.user.photoUrl,
+        },
+      ],
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `${session.user.name}`,
+      description:
+        "เชื่อมต่อ แบ่งปัน และสร้างความทรงจำกับเพื่อนๆ ของคุณบนแพลตฟอร์มโซเชียลมีเดียที่ปลอดภัยและเป็นมิตร",
+      images: [session.user.photoUrl],
+    },
+  };
+}
 
 export default function Page({ params }: { params: { id: string } }) {
   const { id } = params;
