e3f6413e3a17d16bf17fbd9574be6dade6b790d1
khanakorn <khanakorn01@gmail.com>
2025-05-06 19:42:20 +0700

    ad ui

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,6 @@
-import { icons } from "lucide-react";
 import IndexProfile from "../../feature/profille/IndexProfile";
-import { useSession } from "next-auth/react";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../../../lib/authOptions";
 
-export default function Page({ params }: { params: { id: string } }) {
+export default function Page({ params }: { params: { id: any } }) {
   const { id } = params;
   return <IndexProfile id={id} />;
 }
