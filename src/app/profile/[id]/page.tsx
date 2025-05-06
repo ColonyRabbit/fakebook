6de8b6c839ec57a2c6dd54b742ea1d77bb146efe
khanakorn <khanakorn01@gmail.com>
2025-05-06 19:44:02 +0700

    ad ui

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,6 +1,6 @@
 import IndexProfile from "../../feature/profille/IndexProfile";
 
-export default function Page({ params }: { params: { id: any } }) {
+export default function Page({ params }: { params: { id: string } }) {
   const { id } = params;
   return <IndexProfile id={id} />;
 }
